692a73e8f9220058e40211a1ace10e4ff4b702bc
Sergio Yamuza <syamuza@sopinet.com>
2014-03-13 13:39:10 +0100

    datepicker and chosen.jquery to sb-admin.js

--- Resources/public/js/sb-admin.js
+++ Resources/public/js/sb-admin.js
@@ -1,8 +1,21 @@
 $(function() {
-	
-   // $('#side-menu').metisMenu();
 
-		 $('.inputDate').datepicker();
+		// Datepicker for input type 
+		 $('.inputDate').datepicker({
+			 language:"es"
+		 });
+		 
+		 // Listing matches countries while typing
+		    var config = {
+		    	      '.chosen-select'           : {},
+		    	      '.chosen-select-deselect'  : {allow_single_deselect:true},
+		    	      '.chosen-select-no-single' : {disable_search_threshold:10},
+		    	      '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
+		    	      '.chosen-select-width'     : {width:"95%"}
+		    	    }
+		    	    for (var selector in config) {
+		    	      $(selector).chosen(config[selector]);
+		    	    }
 });
 
 //Loads the correct sidebar on window load,
